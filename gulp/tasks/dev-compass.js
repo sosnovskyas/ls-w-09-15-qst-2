34154eb2b041d9ad1c66bc3d518f90a5869eb72f
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2015-10-28 13:52:43 +0300

    update

--- gulp/tasks/dev-compass.js
+++ gulp/tasks/dev-compass.js
@@ -8,20 +8,20 @@ var concat = require('gulp-concat');
 var browserSync  = require('browser-sync');
 
 gulp.task('dev-compass', function() {
-    gulp.src(config.src)
-        .pipe(compass({
-            css: config.dest,
-            sass: config.sass,
-            image: config.image,
-            generated_images_path: config.generated_images_path,
-            sourcemap: config.sourcemap
-        }))
-        .on('error', function(error) {
-            // Would like to catch the error here
-            console.log(error);
-            this.emit('end');
-        })
-        .pipe(gulp.dest(config.dest))
-        .pipe(browserSync.reload({stream:true}))
-    ;
+  gulp.src(config.src)
+    .pipe(compass({
+      css: config.dest,
+      sass: config.sass,
+      image: config.image,
+      generated_images_path: config.generated_images_path,
+      sourcemap: config.sourcemap
+    }))
+    .on('error', function(error) {
+      // Would like to catch the error here
+      console.log(error);
+      this.emit('end');
+    })
+    .pipe(gulp.dest(config.dest))
+    .pipe(browserSync.reload({stream:true}))
+ ;
 });
